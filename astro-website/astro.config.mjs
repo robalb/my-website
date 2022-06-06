6dd0b03a11b98982a196a3c21749aba115bc931e
robalb <laptophp@ventafridda.com>
2022-06-06 19:21:23 +0200

    added starter astro blog

---
+++ astro-website/astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [preact(), react()]
+});
