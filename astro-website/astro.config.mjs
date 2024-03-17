f6c3d9c1abeb160780f898b1faffff5fd6027f4f
robalb <11277482+robalb@users.noreply.github.com>
2024-03-17 20:27:30 +0100

    working on mdx migration

--- astro-website/astro.config.mjs
+++ astro-website/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import svelte from "@astrojs/svelte";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), mdx()],
   markdown: {
     // Can be 'shiki' (default), 'prism' or false to disable highlighting
     //not as cool as https://www.gatsbyjs.com/plugins/gatsby-remark-prismjs/
