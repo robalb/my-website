6176eb2da15d313cb184fb27fa7b0c0a894b5307
robalb <11277482+robalb@users.noreply.github.com>
2024-03-18 10:50:38 +0100

    added feed rss

--- astro-website/src/pages/feed.xml.js
+++ astro-website/src/pages/feed.xml.js
@@ -3,36 +3,40 @@ import { getCollection } from 'astro:content';
 
 
 export async function GET(context) {
-
-  //import all pages
-  let posts = import.meta.glob('./posts/*.{md,mdx}');
-  let items = [];
+  //configuration
+  let icon_url = "https://halb.it/favicon.ico"
+  let logo_url = "https://halb.it/favicon.svg"
+  let language = "en-us"
+  let feed_title = "Robalb's blog"
+  let feed_description = "Articles and CTF writeups"
+  let author_name = "Alberto Ventafridda"
   const defaults = {
     title: "missing title",
     description: "",
   };
+  let last_update = new Date().toISOString()
 
+
+  //import all pages
+  let posts = import.meta.glob('./posts/*.{md,mdx}');
+  let items = [];
   for (const path in posts) {
     let mod = await posts[path]();
     let front = mod.frontmatter
     let item = {
       title: (front.title || defaults.title),
       description: front.description || defaults.description,
-      link: front.permalink || mod.url,
-      pubDate: front.publishDate || front.publishdate,
-      content: "CONT"
+      link: front.permalink || (context.site + mod.url.slice(1)),
+      pubDate: front.publishDate || front.publishdate
     }
     items.push(item)
-    // console.log([path, "URL",  mod.url, mod.frontmatter, mod])
   }
-
-  console.log(items)
 
   return rss({
     // `<title>` field in output xml
-    title: 'Robalb\'s blog',
+    title: feed_title,
     // `<description>` field in output xml
-    description: 'Articles and CTF writeups',
+    description: feed_description,
     // Pull in your project "site" from the endpoint context
     // https://docs.astro.build/en/reference/api-reference/#contextsite
     site: context.site,
@@ -40,6 +44,14 @@ export async function GET(context) {
     // See "Generating items" section for examples using content collections and glob imports
     items: items,
     // (optional) inject custom xml
-    customData: `<language>en-us</language>`,
+    customData: `
+<language>${language}</language>
+<logo>${logo_url}</logo>
+<icon>${icon_url}</icon>
+<updated>${last_update}</updated>
+<author>
+  <name>${author_name}</name>
+</author>
+`,
   });
 }
